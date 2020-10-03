c4962cf44c5462101f7d2f4e4a54a2354fae768a
jmaydew <jmaydew@student.42.fr>
2020-10-03 19:13:07 +0200

    Updating custom_atoi (ongoing) to manage wrong inputs errors

--- parsing/parse_utils_1.c
+++ parsing/parse_utils_1.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include <limits.h>
 
 int
 	ft_strcmp(const char *s1, const char *s2)
@@ -54,4 +55,43 @@ void
 			*text = replace;
 		text++;
 	}
+}
+
+int
+	minirt_atoi(char *s)
+	{
+
+	}
+static int	ft_check_sign(char c)
+{
+	if (c == '-')
+		return (-1);
+	else
+		return (1);
+}
+
+int			ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	int		power;
+	double	res;
+
+	power = 0;
+	i = 0;
+	res = 0;
+	if ((sign = -1) && (*str == '-'))
+		str++;
+	else if (*str == '+')
+		str++;
+	while (str[power] <= '9' && str[power] >= '0')
+		power++;
+	while (i < power)
+	{
+		res = res * 10 + ((str[i] - 48));
+		if ((res * sign) >  INT_MAX || (res * sign) < INT_MAX)
+			return (-1);
+		i++;
+	}
+	return (res * sign);
 }
